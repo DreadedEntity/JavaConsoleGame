3b0f84249dc9fc80aa06b99e31ff41f788c925e8
Carsie Mundy <Cmundy12@yahoo.com>
2017-03-25 14:52:51 -0400

    Not sure how branching works in sourcetree so just did a little work and commiting for a test

--- src/rooms/RoomBase.java
+++ src/rooms/RoomBase.java
@@ -5,6 +5,6 @@ import java.util.ArrayList;
 import game_util.EnemyContainer;
 
 public class RoomBase {
-	private int type;
-	private ArrayList<EnemyContainer> enemies;
+	protected int type;
+	protected ArrayList<EnemyContainer> enemies;
 }
--- src/rooms/Test.java
+++ src/rooms/Test.java
@@ -1,5 +1,12 @@
 package rooms;
 
+import java.util.ArrayList;
+
+import game_util.EnemyContainer;
+
 public class Test extends RoomBase {
-	
+	public Test(int type, ArrayList<EnemyContainer> enemies) {
+		this.type = type;
+		this.enemies = enemies;
+	}
 }
